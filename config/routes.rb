ceec07f8644c6e2dacc1ec1ce82aee0a6166e32b
tkmiya34 <tkmiya34@gmail.com>
2019-12-04 20:40:47 +0900

    ユーザ画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'users#index'
 
   namespace :api, format: 'json' do
     resources :users
